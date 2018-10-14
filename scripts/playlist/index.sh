027131322f3508cc30b1ca4f9240eee5ea9c81a9
Takyiu Lo <takyiulo@gmail.com>
2018-10-14 16:58:26 -0400

    update index curl script for playlist

--- scripts/playlist/index.sh
+++ scripts/playlist/index.sh
@@ -1,1 +1,13 @@
 #!/bin/sh
+
+API="https://www.googleapis.com/oauth2/v4/token?"
+
+REDIRECT_URI="&redirect_uri=http://localhost:7165/?redirect=oauthcallback"
+GRANT_TYPE="&grant_type=authorization_code"
+
+curl "${API}${CODE}${CLIENT_ID}${CLIENT_SECRET}${REDIRECT_URI}${GRANT_TYPE}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/x-www-form-urlencoded"
+
+echo
